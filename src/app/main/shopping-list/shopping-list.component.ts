d1254162b91a4864575643d9de6708318247eea8
natuparth <parth.natu01@gmail.com>
2020-03-27 22:13:58 +0530

    minor fix in shopping list component

--- src/app/main/shopping-list/shopping-list.component.ts
+++ src/app/main/shopping-list/shopping-list.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from "@angular/core";
-import { CrudService } from "src/app/Services/crudService/crud.service";
-import { Observable } from "rxjs";
-import { Item } from "src/app/models/item.model";
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/Services/crudService/crud.service';
+import { Observable } from 'rxjs';
+import { Item } from 'src/app/models/item.model';
 
 @Component({
-  selector: "app-shopping-list",
-  templateUrl: "./shopping-list.component.html",
-  styleUrls: ["./shopping-list.component.css"]
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
   Items: Observable<Item[]>;
@@ -17,7 +17,6 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.showSpinner = true;
-    console.log("ngOnInitCalled");
     this.crudService.getShoppingList().subscribe(data => {
       this.items = data;
       this.getShoppingListUpdated(5);
@@ -26,25 +25,12 @@ export class ShoppingListComponent implements OnInit {
 
   getShoppingListUpdated(noOfDays: number = 5) {
     this.itemsList = [];
-    console.log(this.itemsList);
     this.items.forEach(data => {
-      // tslint:disable-next-line: max-line-length
-      console.log(
-        data.quantity -
-          (data.consumptionPerDay *
-            (new Date(new Date().getTime() + noOfDays * 1000 * 3600 * 24) -
-              new Date(data.date).getTime())) /
-            (1000 * 3600 * 24)
-      );
+    const cutOffDays = (new Date(new Date().getTime() + noOfDays * 1000 * 3600 * 24)) ;
       // tslint:disable-next-line: max-line-length
       if (
-        data.quantity -
-          (data.consumptionPerDay *
-            (new Date(new Date().getTime() + noOfDays * 1000 * 3600 * 24) -
-              new Date(data.date).getTime())) /
-            (1000 * 3600 * 24) <=
-        0
-      ) {
+          data.quantity - ( data.consumptionPerDay * (cutOffDays.getTime() - (new Date(data.date).getTime())) / (1000 * 3600 * 24)) <= 0
+       ) {
         this.itemsList.push(data);
       }
     });
