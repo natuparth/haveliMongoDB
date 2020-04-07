1543d765b85c7b21d15561fb85bde44862c84304
natuparth <parth.natu01@gmail.com>
2020-04-07 19:08:53 +0530

    changed the user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-
+const expense = require('./expense').schema;
 const uniqueValidator = require('mongoose-unique-validator');
 const userSchema = mongoose.Schema({
   email : { type : String , required: true , unique: true},
   password : { type : String , required : true},
   name : { type : String , required : true},
   profilePicId : { type : Number },
-  groupId : {type : String }
+  groupId : {type : String },
+  expenses: [expense]
 
 });
 
