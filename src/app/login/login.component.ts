44b52d137d077af2eb2bb8878921e7e85f8f342d
Rahul10111 <rahulprasad10111@gmail.com>
2020-05-09 19:15:58 +0530

    Added some  animation :p

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { CrudService } from 'src/app/Services/crudService/crud.service';
 import { Subject } from 'rxjs';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HostListener } from "@angular/core";
 
 @Component({
   selector: 'app-login',
@@ -17,6 +18,13 @@ export class LoginComponent implements OnInit {
   isAuthenticated: boolean;
   message:String;
   login_flag:boolean;
+  in_min = [0, 0];
+  in_max = [0, 0];
+  out_min = [-50,-40];
+  out_max = [50,40];
+  rotateAnimationValue = [0,0,0,0];
+  screenHeight: number;
+  screenWidth: number;
   constructor( private authService: AuthService, private router: Router) {
     this.authService.userAuthListener = new Subject<string>();
 
@@ -26,6 +34,7 @@ export class LoginComponent implements OnInit {
     if(localStorage.getItem('userName') != null && localStorage.getItem('serverDown')!=null) {
        this.router.navigate(['home']);
     }
+    this.getScreenSize();
   }
   validateCredentials(values: any){
     this.authService.login(values);
@@ -40,5 +49,23 @@ export class LoginComponent implements OnInit {
         }
     });
   }
+  @HostListener('mousemove', ['$event'])onhover(e?){
+    let cursoPosX = (e.pageX+1)-this.screenWidth/2;
+    let cursoPosY = (e.pageY+1)-this.screenHeight/2;
+    this.rotateAnimationValue[0]= Math.round((cursoPosX - this.in_min[0]) * (this.out_max[0] - this.out_min[0]) / (this.in_max[0] - this.in_min[0]) + this.out_min[0]);
+    this.rotateAnimationValue[1]= Math.round((cursoPosY - this.in_min[1]) * (this.out_max[0] - this.out_min[0]) / (this.in_max[1] - this.in_min[1]) + this.out_min[0]);
+    this.rotateAnimationValue[2]= Math.round((cursoPosX - this.in_min[0]) * (this.out_max[1] - this.out_min[0]) / (this.in_max[0] - this.in_min[0]) + this.out_min[1]);
+    this.rotateAnimationValue[3]= Math.round((cursoPosY - this.in_min[1]) * (this.out_max[1] - this.out_min[0]) / (this.in_max[1] - this.in_min[1]) + this.out_min[1]);
+    // console.log(this.rotateAnimationValue);
+  }
 
+  
+  @HostListener('window:resize', ['$event'])
+    getScreenSize(event?) {
+          this.screenHeight = window.innerHeight;
+          this.screenWidth = window.innerWidth;
+          this.in_min = [0-this.screenWidth/2 , 0-this.screenHeight/2];
+          this.in_max = [this.screenWidth/2 , this.screenHeight/2];
+          // console.log(this.screenHeight, this.screenWidth);
+    }
 }
